5faf750d9bc0d6c44bc0eea27b5231cd904942da
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-09-05 21:09:14 +0100

    Fix XGB generation

--- R/gen_xgb.R
+++ R/gen_xgb.R
@@ -3,20 +3,50 @@
 #' @export
 
 
-method_xgboost <- function(data.set, odds.results, FOLD_DATA, XGB) {
+gen_xgb <- function(data.set, ..., cName = "res", logs = FALSE, fold.info = c(10, 7)) {
+  
+  # Initialise additional user input
+  addInput <- list(...)
+  XGB <- if (addInput$XGB %>% is.null %>% `!`()) addInput$XGB else list()
+  iNames <- if (XGB %>% length %>% `>`(0)) XGB %>% names else c()
+  if ("DEPTH" %in% iNames %>% `!`()) XGB$DEPTH <- 0.5
+  if ("ETA" %in% iNames %>% `!`()) XGB$ETA <- 0.2
+  if ("GAMMA" %in% iNames %>% `!`()) XGB$GAMMA <- 1
+  if ("ROUNDS" %in% iNames %>% `!`()) XGB$ROUNDS <- 20000
+  
+  # Calculate folds
+  FOLD_DATA <- data.set %>%
+    mltools::check_folds(
+      cName = cName,
+      fold.info = fold.info
+    )
   
   # Initialise bestAcc
   bestAcc <- 0
   
   # Convert to integer values
   new.results <- data.set
-  new.results$res %<>% as.factor
-  newLabels <- new.results$res %>% levels
-  new.results$res %<>% as.factor %>% as.integer %>% `-`(1)
+  
+  # Convert classes to factors
+  new.results[[cName]] %<>% 
+    as.factor
+  
+  # Get the actual label names
+  newLabels <- new.results %>% 
+    `[[`(cName) %>%
+    levels
+  
+  # Convert factor to a numeric value
+  new.results[[cName]] %<>%
+    as.integer %>%
+    `-`(1)
   
   # Convert new results to integer values
   boundLen <- 4
-  new.results %<>% footballstats::scaled_to_discrete(boundLen = boundLen)
+  new.results %<>% 
+    mltools::scaled_to_discrete(
+      boundLen = boundLen
+    )
   
   # How many folds per test set
   foldGroupLen <- FOLD_DATA$NUM - FOLD_DATA$PER
@@ -24,8 +54,10 @@ method_xgboost <- function(data.set, odds.results, FOLD_DATA, XGB) {
   # Loop through all the folds
   foldInd <- 1:(FOLD_DATA$NUM)
   
-  # Initialise confusion matrix stats
-  totalStats <- footballstats::init_conf_stats()
+  # Initialise results vectors
+  results <- c()
+  totalStats <- list()
+  bestResult <- 0
   
   # Start logging
   cat("\n ## XG CV :")
@@ -45,12 +77,9 @@ method_xgboost <- function(data.set, odds.results, FOLD_DATA, XGB) {
     )
     
     # Set up train and test data
-    train.data <- new.results[
-      FOLD_DATA$FOLDS[-filterTest] %>% purrr::flatten_int(), ]
-    test.data <- new.results[
-      FOLD_DATA$FOLDS[filterTest] %>% purrr::flatten_int(), ]
-    new.odds <- odds.results[
-      FOLD_DATA$FOLDS[filterTest] %>% purrr::flatten_int(), ]
+    train.data <- new.results[FOLD_DATA$FOLDS[-filterTest] %>% purrr::flatten_int(), ]
+    test.data <- new.results[FOLD_DATA$FOLDS[filterTest] %>% purrr::flatten_int(), ]
+    foldint <- FOLD_DATA$FOLDS[filterTest] %>% purrr::flatten_int()
     
     # Create labels
     trainLabels <- train.data$res
@@ -58,13 +87,13 @@ method_xgboost <- function(data.set, odds.results, FOLD_DATA, XGB) {
     
     # Create sparse matrix of training data
     sparse.train <- train.data %>%
-      footballstats::create_sparse(
+      mltools::create_sparse(
         boundLen = boundLen
       )
     
     # Create sparse test matrix
     sparse.test <- test.data %>%
-      footballstats::create_sparse(
+      mltools::create_sparse(
         boundLen = boundLen
       )
     
@@ -84,24 +113,49 @@ method_xgboost <- function(data.set, odds.results, FOLD_DATA, XGB) {
     
     # Make predictions
     p <- predict(xgb, sparse.test)
+    realVec <- newLabels[testLabels %>% `+`(1)] %>% factor(levels = newLabels)
+    predVec <- newLabels[p %>% `+`(1)] %>% factor(levels = newLabels)
     
-    # Get metrics from confusion table
-    totalStats %<>% footballstats::append_conf_stats(
-      new.odds = new.odds,
-      Actual.score = newLabels[testLabels %>% `+`(1)] %>% factor(levels = newLabels),
-      Predicted.score = newLabels[p %>% `+`(1)] %>% factor(levels = newLabels)
-    )
+    # Need to calculate the best score
+    confResults <- realVec %>% 
+      mltools::confusion_stats(
+        Predicted.score = predVec
+      )
     
-    # Save the best model
-    if (totalStats$totAcc[i] > bestAcc) {
-      bestAcc <- totalStats$totAcc[i]
+    # Append list results on
+    totalStats$totAcc %<>% c(confResults[1])
+    totalStats$totAccL %<>% c(confResults[2])
+    totalStats$totAccU %<>% c(confResults[3])
+    totalStats$totD %<>% c(confResults[4])
+    totalStats$totL %<>% c(confResults[5])
+    totalStats$totW %<>% c(confResults[6])
+    
+    # Store the best result
+    if (confResults[1] > bestAcc) {
+      bestAcc <- confResults[1]
       bestXGB <- xgb
     }
     
-    rm(xgb, train.data, test.data, new.odds)
+    # Append the results on
+    results %<>% c(
+      list(
+        actual = realVec,
+        predicted = predVec,
+        foldint = foldint
+      ) 
+      %>% list
+    )
+    
+    # Remove large data matricies
+    rm(xgb, train.data, test.data)
   }
   
-  # Return neural network plus results
-  return(c(list(model = bestXGB), totalStats))
+  # Return XGB plus results
+  return(
+    list(
+      model = bestXGB,
+      results = results,
+      totalStats = totalStats
+    )
+  )
 }
-
