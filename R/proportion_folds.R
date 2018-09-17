ab2c8df6b4a17530e2b5528d273f1a6a4b798fc5
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-09-17 11:21:26 +0100

    Create fold data with better, proportioned class sampling..

---
+++ R/proportion_folds.R
@@ -0,0 +1,66 @@
+#' @title Proportion Folds
+#'
+#' @export
+
+
+proportion_folds <- function(vec, folds) {
+  # Proportion the split by counting class type
+  classCounts <- vec %>% table
+
+  # Get names
+  cNames <- classCounts %>% names
+
+  # Convert to numeric
+  classCounts %<>% as.numeric
+
+  # Get remainder modulo folds
+  remainders <- classCounts %% folds
+
+  # Get integer counts
+  intCounts <- classCounts %/% folds
+
+  myCounts <- lapply(
+    X = 1:3,
+    FUN = function(x) {
+      c(intCounts[x] %>% rep(folds - remainders[x]), intCounts[x] %>% `+`(1) %>% rep(remainders[x]))
+    }
+  )
+
+  cTypes <- lapply(
+    X = cNames,
+    FUN = function(x) vec %>% `==`(x) %>% which
+  )
+
+  names(cTypes) <- cNames
+
+  allFolds <- c()
+
+  for (i in 1:folds) {
+    singleCounts <- myCounts %>%
+      purrr::map(i) %>%
+      as.integer
+
+    # Initialise allIndexes
+    allIndexes <- c()
+
+    # Loop over each type
+    for (j in 1:3) {
+      # Sample it
+      indexes <- cTypes[[j]] %>% sample(size = singleCounts[j])
+
+      # Then take the set difference
+      cTypes[[j]] %<>% setdiff(indexes)
+
+      # Combine all indexes together
+      allIndexes %<>% c(indexes)
+    }
+
+    allFolds %<>% c(allIndexes %>% list)
+  }
+
+  # Append names
+  names(allFolds) <- paste0("Fold", append_zeros(folds))
+
+  # Return list back
+  return(allFolds %>% lapply(sort))
+}
