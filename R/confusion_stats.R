662bb7b227d30ab63680af8d7c7dfbbf7a8dd09e
Niall Tyndall <niall.tyndall@cyberlytic.com>
2019-05-19 10:41:53 +0100

    Update general confusion matrix for a 2-class system

--- R/confusion_stats.R
+++ R/confusion_stats.R
@@ -3,19 +3,56 @@
 #' @export
 
 
-confusion_stats <- function(Actual.score, Predicted.score) {
+confusion_stats <- function(Actual.score, Predicted.score, positiveC = "W") {
+
   # Build a table of results
   resultTable <- table(Actual.score, Predicted.score)
-  rt <- caret::confusionMatrix(data = resultTable)
-
-  # Refine stats here from the confusion matrix
-  oStats <- rt$overall[c('Accuracy', 'AccuracyLower', 'AccuracyUpper')] %>% as.double
-  oSens <- if (resultTable %>% nrow %>% `>`(2)) {
-    rt$byClass[1:3, 'Sensitivity'] %>% as.double
+  
+  rt <- if (resultTable %>% nrow %>% `==`(2)) {
+    resultTable %>% caret::confusionMatrix(positive = positiveC)
   } else {
-    rt$byClass['Sensitivity'] %>% as.double %>% rep(3)
+    resultTable %>% caret::confusionMatrix()
   }
 
+  # Refine stats here from the confusion matrix
+  oSens <- if (resultTable %>% nrow %>% `>`(2)) {
+    
+    oSens <- rt$byClass[1:(resultTable %>% nrow), 'Sensitivity'] %>%
+      as.double
+  
+    # Get individual class statistics
+    labelStats <- oSens %>% as.list
+    names(labelStats) <- "tot" %>% 
+      paste0(
+        rt$byClass %>% 
+          row.names %>% 
+          strsplit("Class: ") %>%
+          purrr::map(2) %>% 
+          purrr::flatten_chr()
+      )
+  } else {
+    posSens <- rt$byClass['Sensitivity'] %>% as.double
+    labelStats <- c(posSens, 1 - posSens)
+    names(labelStats) <- "tot" %>%
+      paste0(
+        c(
+          rt$positive,
+          rt$table[1, ] %>% names %>% setdiff(rt$positive)
+        )
+      )
+  }
+
+  # Create totalStats object
+  totalStats <- rt$overall[c('Accuracy', 'AccuracyLower', 'AccuracyUpper')] %>% 
+    as.double %>%
+    as.list
+  names(totalStats) <- paste0("totAcc", c("", "L", "U"))
+
   # Return the statistics
-  return(c(oStats, oSens))
+  return(
+    list(
+      totalStats = totalStats %>% c(labelStats),
+      cm = rt
+    )
+  )
 }
